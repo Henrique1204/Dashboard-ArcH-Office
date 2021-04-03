e2da760882599d5908345ae76081c55e4bcfea55
Paulo <pauloh16.pdr@gmail.com>
2021-04-03 15:05:42 -0300

    Criar componente para input de formularios

---
+++ src/Componentes/Form/Input/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import estilos from './Input.module.css';
+
+const Input = ({ label, type, name, valor, onChange, erro, onBlur }) => {
+    return (
+        <div className={estilos.wrapper}>
+            <label htmlFor={name} className={estilos.label} >{label}</label>
+
+            <input
+                id={name}
+                name={name}
+                className={estilos.input}
+                type={type}
+                value={valor}
+                onChange={onChange}
+                onBlur={onBlur}
+            />
+
+            { erro && <small className={estilos.erro}>{erro}</small> }
+        </div>
+    );
+};
+
+export default Input;
--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import estilos from "./Login.module.css";
+import Input from '../../Componentes/Form/Input/Input.js';
 import useForm from '../../Hooks/useForm.js';
 
 const Login = () => {
@@ -11,32 +12,14 @@ const Login = () => {
             <h1>Login</h1>
 
             <form>
-                <label htmlFor="usuario">Usuário:</label>
                 <div className={estilos.containerInput}>
-                    <input
-                        type="text"
-                        name="usuario"
-                        value={usuario.value}
-                        onChange={usuario.onChange}
-                        onBlur={usuario.onBlur}
-                    />
-
+                    <Input label="Usuário:" type="text" name="usuario" {...usuario} />
                     <i className="fa fa-user"></i>
-                    { usuario.erro && <small className={estilos.erro}>{usuario.erro}</small> }
                 </div>
 
-                <label htmlFor="senha">Senha:</label>
                 <div className={estilos.containerInput}>
-                    <input
-                        type="password"
-                        name="senha"
-                        value={senha.value}
-                        onChange={senha.onChange}
-                        onBlur={senha.onBlur}
-                    />
-    
+                    <Input label="Senha:" type="password" name="senha" {...senha} />
                     <i className="fa fa-unlock-alt"></i>
-                    { senha.erro && <small className={estilos.erro}>{senha.erro}</small> }
                 </div>
 
                 <button>Entrar</button>
