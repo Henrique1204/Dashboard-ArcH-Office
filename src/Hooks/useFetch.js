3d116d236cedfbb862be83f1483faa47624787ef
Paulo <pauloh16.pdr@gmail.com>
2021-04-03 15:36:09 -0300

    Implementar logica de login com db estatico

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -15,6 +15,7 @@ const useFetch = () => {
 
             response = await fetch(url, options);
             json = await response.json();
+            console.log(json);
 
             if (response.ok === false) throw new Error(json.message);
         } catch(erro) {
--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -2,16 +2,37 @@ import React from "react";
 import estilos from "./Login.module.css";
 import Input from '../../Componentes/Form/Input/Input.js';
 import useForm from '../../Hooks/useForm.js';
+import db from '../../db.json';
 
 const Login = () => {
     const usuario = useForm();
     const senha = useForm();
+    const [erro, setErro] = React.useState(null);
+    const [loading, setLoading] = React.useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        if (!usuario.validar() && !senha.validar()) setErro('Dados inválidos!');
+
+        const { usuarios } = db;
+
+        const isExiste = usuarios.find((usuarioDB) => {
+            return usuarioDB.usuario === usuario.valor && usuarioDB.senha === senha.valor;
+        });
+
+        window.localStorage.removeItem('usuario');
+        if (isExiste) window.localStorage.setItem('usuario', JSON.stringify(isExiste));
+
+        setTimeout(() => setLoading(false), 300);
+    };
 
     return (
         <section className={estilos.sessao}>
             <h1>Login</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className={estilos.containerInput}>
                     <Input label="Usuário:" type="text" name="usuario" {...usuario} />
                     <i className="fa fa-user"></i>
@@ -22,7 +43,8 @@ const Login = () => {
                     <i className="fa fa-unlock-alt"></i>
                 </div>
 
-                <button>Entrar</button>
+                <button disabled={loading}>Entrar</button>
+                { erro && <small className={estilos.erro}>{erro}</small> }
             </form>
         </section>
     );
