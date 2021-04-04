32894d79512c5fb0b134024a3bfa9601cfc8f51d
Paulo <pauloh16.pdr@gmail.com>
2021-04-04 18:18:26 -0300

    Implementar menu para navegacao

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import estilos from './Header.module.css';
 import logoImg from '../../Assets/logo.png';
 import ModalUsuario from '../ModalUsuario/ModalUsuario.js';
+import MenuNavegacao from '../MenuNavegacao/MenuNavegacao.js';
 import { useLocation } from 'react-router';
 
 const Header = () => {
@@ -11,24 +12,28 @@ const Header = () => {
     if (pathname === '/login' || pathname === '/cadastrar') return null;
 
     return (
-        <header className={estilos.header}>
-            <div className={`${estilos.containerHeader} container`}>
-                <div className={estilos.logo}>
-                    <img src={logoImg} alt="ArcH Office - Logo"/>
+        <>
+            <header className={estilos.header}>
+                <div className={`${estilos.containerHeader} container`}>
+                    <div className={estilos.logo}>
+                        <img src={logoImg} alt="ArcH Office - Logo"/>
+                    </div>
+
+                    <div className={estilos.busca}>
+                        <input type="text" placeholder="Pesquise aqui"/>
+                        <i className="fa fa-search"></i>
+                    </div>
+
+                    <button className={estilos.usuario} onClick={() => setModalAtivo(!modalAtivo)}>
+                        <span className={estilos.notificacao}>2</span>
+                    </button>
+
+                    { modalAtivo && <ModalUsuario setAtivo={setModalAtivo} /> }
                 </div>
+            </header>
 
-                <div className={estilos.busca}>
-                    <input type="text" placeholder="Pesquise aqui"/>
-                    <i className="fa fa-search"></i>
-                </div>
-
-                <button className={estilos.usuario} onClick={() => setModalAtivo(!modalAtivo)}>
-                    <span className={estilos.notificacao}>2</span>
-                </button>
-
-                { modalAtivo && <ModalUsuario setAtivo={setModalAtivo} /> }
-            </div>
-        </header>
+            { pathname !== '/' && <MenuNavegacao /> }
+        </>
     );
 };
 
---
+++ src/Componentes/MenuNavegacao/MenuNavegacao.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import estilos from './MenuNavegacao.module.css';
+import { NavLink } from 'react-router-dom';
+
+const MenuNavegacao = () => {
+    const [ativo, setAtivo] = React.useState(false);
+
+    return (
+        <nav className={estilos.menu}>
+            <button
+                className={`${estilos.hamburguer} ${(ativo) ? estilos.ativo : ''}`}
+                onClick={() => setAtivo(!ativo)}
+            >
+                <span className={estilos.barrinha}></span>
+                <span className={estilos.barrinha}></span>
+                <span className={estilos.barrinha}></span>
+            </button>
+
+            <ul className={`${estilos.navegacao} ${(ativo) ? estilos.ativo : ''}`}>
+                <li> 
+                    <NavLink
+                        to="/usuarios"
+                        activeClassName={estilos.ativo}
+                        className={estilos.inativo}
+                    >
+                        usuários
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/relatorios"
+                        activeClassName={estilos.ativo}
+                        className={estilos.inativo}
+                    >
+                        relatórios
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/conteudos"
+                        activeClassName={estilos.ativo}
+                    >
+                        conteúdos
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/eventos"
+                        activeClassName={estilos.ativo}
+                        className={estilos.inativo}
+                    >
+                        eventos
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/servicos"
+                        activeClassName={estilos.ativo}
+                        className={estilos.inativo}
+                    >
+                        serviços
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default MenuNavegacao;
