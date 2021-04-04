3a3249886372ece6e39760781fa0d88013cc97bf
Paulo <pauloh16.pdr@gmail.com>
2021-04-04 19:55:50 -0300

    Implementar sugestao para pesquisa

--- src/Views/Usuarios/Usuarios.js
+++ src/Views/Usuarios/Usuarios.js
@@ -5,19 +5,43 @@ import CardUsuarios from '../../Componentes/CardUsuarios/CardUsuarios.js';
 import db from '../../db.json';
 
 const Usuarios = () => {
+    const [usuarios, setUsuarios] = React.useState(null);
+
+    React.useEffect(() => {
+        if (window.localStorage.getItem('usuario')) {
+            const usuarioLogado = JSON.parse(window.localStorage.getItem('usuario'));
+
+            setUsuarios(() => {
+                if (!db.usuarios.find(({ usuario }) => usuario === usuarioLogado.usuario )) {
+                    return [...db.usuarios, usuarioLogado];
+                }
+
+                return db.usuarios;
+            });
+        } else {
+            setUsuarios(db.usuarios);
+        }
+    }, []);
+
     return (
         <section className={estilos.usuarios}>
             <CabecalhoSessao titulo="Usuários">
                 <form className={estilos.busca}>
-                    <input name="buscaUsuarios" placeholder="Buscar usuários" />
+                    <input name="buscaUsuarios" placeholder="Buscar usuários" list="usuarios" />
                     <button>
                         <i className="fa fa-search"></i>
                     </button>
+
+                    <datalist id="usuarios">
+                        { usuarios && usuarios.map(({ usuario }) => (
+                            <option value={usuario}>{usuario}</option>
+                        )) }
+                    </datalist>
                 </form>
             </CabecalhoSessao>
 
             <ul className={`container ${estilos.listaCards}`}>
-                { db.usuarios.map(({ usuario, sexo }, i) => (
+                { usuarios && usuarios.map(({ usuario, sexo }, i) => (
                     <CardUsuarios key={`usuario_${i}`} usuario={usuario} sexo={sexo} />
                 )) }
             </ul>
