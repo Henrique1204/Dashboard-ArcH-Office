30ba541ba5b512f403cd6a0e4369ef0ace730884
Paulo <pauloh16.pdr@gmail.com>
2021-04-05 23:14:59 -0300

    Ajustar componente ModalContainer

--- src/Componentes/ModalContainer/ModalContainer.js
+++ src/Componentes/ModalContainer/ModalContainer.js
@@ -8,6 +8,18 @@ const ModalContainer = ({ titulo, setAtivo, children, classe }) => {
       if (target === fundoRef.current) setAtivo(false);
     }
   
+    React.useEffect(() => {
+      const fecharModalEsc = ({ key }) => {
+        if (key === 'Escape') setAtivo(false);
+      };
+
+      window.addEventListener('keydown', fecharModalEsc);
+
+      return () => {
+        window.removeEventListener('keydown', fecharModalEsc);
+      };
+    }, [setAtivo]);
+  
     return (
       <div className={estilos.Modal} onClick={fecharModal} ref={fundoRef}>
         <div className={`${estilos.CardModal} ${(classe) ? classe : ''}`}>
